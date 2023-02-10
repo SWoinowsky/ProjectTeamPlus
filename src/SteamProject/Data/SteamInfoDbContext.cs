e489818806d3b0177a1b17dbe81cf2e7d083b757
Seth <SethWoinowsky@gmail.com>
2023-02-09 19:40:50 -0800

    Identity db is now set to SteamInfoAuth db created by up script

---
+++ src/SteamProject/Data/SteamInfoDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SteamProject.Data;
+
+public class SteamInfoDbContext : DbContext
+{
+    public SteamInfoDbContext(DbContextOptions<SteamInfoDbContext> options)
+        : base(options)
+    {
+    }
+}

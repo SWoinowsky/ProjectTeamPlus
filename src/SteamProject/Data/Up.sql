4e28ae941dd666982797e7b9619f407742578095
Justin Davis <jdavis19@mail.wou.edu>
2023-02-12 16:06:47 -0800

    Initial state of the branch.

---
+++ src/SteamProject/Data/Up.sql
@@ -0,0 +1,70 @@
+-- Highlight then ctrl+k, ctrl+c to uncomment / recomment
+
+--CREATE DATABASE [SteamInfo];
+
+USE [SteamInfo];
+
+CREATE TABLE [User]
+(
+	[Id]				INT				NOT NULL IDENTITY(1,1) PRIMARY KEY
+	,[AspNetUserId]		NVARCHAR(450)	NOT NULL
+	,[SteamId]			NVARCHAR(50)
+	,[SteamName]		NVARCHAR(50)
+	,[ProfileUrl]		NVARCHAR(100)
+	,[AvatarUrl]		NVARCHAR(100)
+	,[PersonaState]		INT
+	,[PlayerLevel]		INT
+);
+
+CREATE TABLE [Friend]
+(
+	[Id]				INT				NOT NULL IDENTITY(1,1) PRIMARY KEY
+	,[RootId]			INT				NOT NULL
+	,[SteamId]			INT				NOT NULL
+	,[SteamName]		NVARCHAR(50)	NOT NULL
+	,[PersonaState]		INT				NOT NULL
+	,[AvatarUrl]		NVARCHAR(100)	NOT NULL
+	,[LastLogOff]		INT				NOT NULL
+	,[GameExtraInfo]	NVARCHAR(100)
+	,[GameId]			INT
+);
+
+CREATE TABLE [Game]
+(
+	[Id]				INT				NOT NULL IDENTITY(1,1) PRIMARY KEY
+	,[OwnerId]			INT				NOT NULL
+	,[AppId]			INT				NOT NULL
+	,[Name]				NVARCHAR(50)	NOT NULL
+	,[PlayTime]			INT				NOT NULL
+	,[IconUrl]			NVARCHAR(100)	NOT NULL
+	,[LastPlayed]		INT				NOT NULL
+	,[Hidden]			BIT				NOT NULL
+);
+
+CREATE TABLE [GameAchievement]
+(
+	[Id]				INT				NOT NULL IDENTITY(1,1) PRIMARY KEY
+	,[GameId]			INT				NOT NULL
+	,[ApiName]			NVARCHAR(100)	NOT NULL
+	,[DisplayName]		NVARCHAR(50)	NOT NULL
+	,[IconAchievedUrl]	NVARCHAR(100)	NOT NULL
+	,[IconHiddenUrl]	NVARCHAR(100)	NOT NULL
+	,[HiddenFromUsers]	BIT				NOT NULL
+);
+
+CREATE TABLE [UserAchievement]
+(
+	[Id]				INT				NOT NULL IDENTITY(1,1) PRIMARY KEY
+	,[OwnerId]			INT				NOT NULL
+	,[GameId]			INT				NOT NULL
+	,[ApiName]			NVARCHAR(100)	NOT NULL
+	,[DisplayName]		NVARCHAR(50)	NOT NULL
+	,[Achieved]			BIT				NOT NULL
+	,[UnlockTime]		INT				
+);
+
+ALTER TABLE [Friend] ADD CONSTRAINT [Friend_Fk_User] FOREIGN KEY ([RootId]) REFERENCES [User] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE [Game] ADD CONSTRAINT [Game_Fk_User] FOREIGN KEY ([OwnerId]) REFERENCES [User] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE [UserAchievement] ADD CONSTRAINT [UserAchievement_Fk_User] FOREIGN KEY ([OwnerId]) REFERENCES [User] ([Id]) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
---
+++ src/SteamProject/Program.cs
@@ -0,0 +1,95 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using SteamProject.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+const bool localDbSource = true;
+const bool azurePublish = false;
+// Add services to the container.
+
+//Local Connection Strings
+if (localDbSource == true)
+{
+    var connectionString = builder.Configuration.GetConnectionString("AuthenticationConnection") ?? throw new InvalidOperationException("Connection string 'AuthenticationConnection' not found.");
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(connectionString));
+
+    var connectionStringTwo = builder.Configuration.GetConnectionString("SteamInfoConnection") ?? throw new InvalidOperationException("Connection string 'SteamInfoConnection' not found.");
+    builder.Services.AddDbContext<SteamInfoDbContext>(options =>
+        options.UseSqlServer(connectionStringTwo));
+
+}
+
+//Azure Connection Strings
+if (localDbSource == false)
+{
+    if (azurePublish == true)
+    {
+        var connectionString = builder.Configuration.GetConnectionString("SteamInfoAuthConnectionAzure") ?? throw new InvalidOperationException("Connection string 'AuthenticationConnection' not found.");
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
+        var connectionStringTwo = builder.Configuration.GetConnectionString("SteamInfoConnectionAzure") ?? throw new InvalidOperationException("Connection string 'SteamInfoConnection' not found.");
+        builder.Services.AddDbContext<SteamInfoDbContext>(options =>
+            options.UseSqlServer(connectionStringTwo));
+    }
+    else
+    {
+        var stringBuilder = new SqlConnectionStringBuilder(builder.Configuration.GetConnectionString("SteamInfoConnectionAzure"))
+        {
+            Password = builder.Configuration["SteamInfo:DBPassword"]
+        };
+        builder.Services.AddDbContext<SteamInfoDbContext>(options =>
+            options.UseSqlServer(stringBuilder.ConnectionString));
+
+
+        var authStringBuilder = new SqlConnectionStringBuilder(builder.Configuration.GetConnectionString("SteamInfoAuthConnectionAzure"))
+        {
+            Password = builder.Configuration["SteamInfo:DBPassword"]
+        };
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(authStringBuilder.ConnectionString));
+    }
+
+}
+
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();

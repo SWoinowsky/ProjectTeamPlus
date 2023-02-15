68af2f8804c3cee42fbdc8e6ef37d274aba5db56
Seth <SethWoinowsky@gmail.com>
2023-02-15 10:29:46 -0800

    Updated Game table to contains places for descriptions

--- src/SteamProject/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/SteamProject/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -18,7 +18,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
-using SteamProject.Data;
+using SteamProject.Models;
 
 namespace SteamProject.Areas.Identity.Pages.Account
 {

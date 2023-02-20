25a2a22fdf2b6a47f87b588fe85634e346fa1b76
CarlosCortesEscobar <ccortesescobar19@mail.wou.edu>
2023-02-19 20:46:05 -0800

    Achievements modal for library page implemented

--- src/SteamProject/Controllers/SteamController.cs
+++ src/SteamProject/Controllers/SteamController.cs
@@ -13,10 +13,12 @@ namespace SteamProject.Controllers;
 public class SteamController : ControllerBase
 {
     private readonly ISteamService _steamService;
+    private readonly ISteamServices _steamServices;
 
-    public SteamController( ISteamService steamService )
+    public SteamController( ISteamService steamService, ISteamServices steamServices )
     {
         _steamService = steamService;
+        _steamServices = steamServices;
     }
 
     [HttpGet("user")]
@@ -25,5 +27,17 @@ public class SteamController : ControllerBase
         User user = _steamService.GetSteamUser(steamid);
 
         return Ok(user);
+    }
+
+    [HttpGet("achievements")]
+    public ActionResult<AchievementRoot> UserAchievements(string steamid, int appId)
+    {
+        return _steamServices.GetAchievements(steamid, appId);
+    }
+
+    [HttpGet("schema")]
+    public ActionResult<SchemaRoot> GameSchema(int appId)
+    {
+        return _steamServices.GetSchema(appId);
     }
 }

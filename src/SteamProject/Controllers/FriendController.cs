d9f1c1c3978aace3fc6ff80a2420562adb5b3076
Seth <SethWoinowsky@gmail.com>
2023-02-24 15:03:13 -0800

    Adding friend info
     to Friend Page

--- src/SteamProject/Controllers/FriendController.cs
+++ src/SteamProject/Controllers/FriendController.cs
@@ -14,12 +14,14 @@ public class FriendController : Controller
     private readonly UserManager<IdentityUser> _userManager;
     private readonly IUserRepository _userRepository;
     private readonly IGameRepository _gameRepository;
+    private readonly IFriendRepository _friendRepository;
     private readonly ISteamService _steamService;
 
     public FriendController(
         ILogger<FriendController> logger
         ,ISteamService steamService
         ,IUserRepository userRepository
+        ,IFriendRepository friendRepository
         ,IGameRepository gameRepository
         ,UserManager<IdentityUser> userManager
         )
@@ -28,6 +30,7 @@ public class FriendController : Controller
         _steamService = steamService;
         _userRepository = userRepository;
         _gameRepository = gameRepository;
+        _friendRepository = friendRepository;
         _userManager = userManager;
     }
 
@@ -36,18 +39,17 @@ public class FriendController : Controller
     public IActionResult Index( string friendSteamId )
     {
         var id = _userManager.GetUserId( User );
-        var CurrentUser = _userRepository.GetUser( id );
+        var currentUser = _userRepository.GetUser( id );
+        var friend = _friendRepository.GetSpecificFriend( friendSteamId );
 
-        var MyGames = _steamService.GetGames( CurrentUser.SteamId, CurrentUser.Id );
-        var TheirGames = _steamService.GetGames( friendSteamId, 0 );
+        var myGames = _steamService.GetGames( currentUser.SteamId, currentUser.Id );
+        var theirGames = _steamService.GetGames( friendSteamId, 0 );
 
-        var SharedGames = MyGames.Join(TheirGames, g1 => g1.AppId, g2 => g2.AppId, (g1, g2) => g1 ).ToList<Game>();
-        return View( SharedGames );
+        var sharedGames = myGames.Join(theirGames, g1 => g1.AppId, g2 => g2.AppId, (g1, g2) => g1 ).ToList<Game>();
+        return View( sharedGames );
 
 
         // return View( _gameRepository.GetAll().Where( g => g.OwnerId == CurrentUser.Id ).ToList<Game>() );
-
-
     }
     
 }
---
+++ src/SteamProject/Models/ViewModels/FriendsVM.cs
@@ -0,0 +1,9 @@
+
+namespace SteamProject.Models.ViewModels;
+
+public class FriendsVM
+{
+    public List<Game> SharedGames { get; set; }
+    public Friend Friend { get; set; }
+    
+}

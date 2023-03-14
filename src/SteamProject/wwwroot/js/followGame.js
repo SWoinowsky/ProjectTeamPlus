57c8264ca5612bc3f229b04833a1185e0df81a60
Justin Davis <jdavis19@mail.wou.edu>
2023-03-14 14:11:29 -0700

    Fixed a bug in followGame.js where the gameName was actually the entire div element, and was being used improperly. Corrected it to use the actual game name.

--- src/SteamProject/wwwroot/js/followGame.js
+++ src/SteamProject/wwwroot/js/followGame.js
@@ -1,19 +1,36 @@
 function setFollowedGame(name)
 {
-
     var gameName = document.getElementById(name);
     var gameId = gameName.getAttribute('value');
     $.ajax({
         type: "POST",
         dataType: "text",
         url: `/api/Steam/follow?id=${gameId}`,
-        success: followGame(gameName, gameId),
+        success: followGame(name, gameId),
         error: errorOnAjax
     })
 }
+
+function setUnfollowGame(name)
+{
+    var gameName = document.getElementById(name);
+    var gameId = gameName.getAttribute('value');
+    $.ajax({
+        type: "POST",
+        dataType: "text",
+        url: `/api/Steam/unfollow?id=${gameId}`,
+        success: unfollowGame(name, gameId),
+        error: errorOnAjax
+    })
+}
+
+function unfollowGame(gameName, gameId)
+{
+    console.log(`${gameName.name} has been unfollowed successfully`);
+}
  
 function followGame(gameName, gameId) {
-    console.log(`${gameName.name} has been followed successfully`);
+    console.log(`${gameName} has been followed/unfollowed successfully`);
 
     var followIcon = document.getElementById(`${gameId} followicon`);
     $(followIcon).toggleClass('bi bi-bookmark-dash-fill');

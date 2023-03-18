10b890ccb9f17bb5e3dccd92d667a333bc5116f7
Justin Davis <jdavis19@mail.wou.edu>
2023-03-18 02:12:30 -0700

    Cleaned up using statements in UserTests.cs that were unused.

--- src/NUnit_Tests/RepoTesting/UserTests/UserTests.cs
+++ src/NUnit_Tests/RepoTesting/UserTests/UserTests.cs
@@ -1,16 +1,6 @@
-using Microsoft.EntityFrameworkCore;
-using Moq;
 using SteamProject.Models;
 using SteamProject.Models.DTO;
 using System.Text.Json;
-using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore;
-using System.Data.Common;
-using System.Diagnostics;
-using System.Net;
-using SteamProject.DAL.Abstract;
-using SteamProject.DAL.Concrete;
-using System;
 
 namespace NUnit_Tests.RepoTesting
 {

5c8cb82f9a1802c310fc7c5e3e0efa7c21ab938a
Justin Davis <jdavis19@mail.wou.edu>
2023-03-17 23:46:55 -0700

    Wrote test to test TakeSteamPOCO.

--- src/NUnit_Tests/RepoTesting/UserTests.cs
+++ src/NUnit_Tests/RepoTesting/UserTests.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using SteamProject.Models;
+using SteamProject.Models.DTO;
+using System.Text.Json;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using System.Data.Common;
@@ -14,16 +16,10 @@ namespace NUnit_Tests.RepoTesting
 {
     public class UserTests
     {
-        private Mock<SteamInfoDbContext> _mockContext;
-        private Mock<DbSet<UserGameInfo>> _mockGameInfoDbSet;
-
-        private List<User> _users;
-        private List<Friend> _friends;
-        private List<UserGameInfo> _userGameInfos;
-        private List<Game> _games;
-
+        private SteamUserPOCO _poco;
         private User MakeValidPerson()
         {
+            
             User newPerson = new User
             {
                 Id = 1,
@@ -40,56 +36,37 @@ namespace NUnit_Tests.RepoTesting
         [SetUp]
         public void Setup()
         {
-            _users = new List<User>
-            {
-                new User
-                {
-                    Id = 1, SteamName = "TestUser1", SteamId = "76561198280399190",
-                    AspNetUserId = "f7b530ec-bf89-45e1-8080-cbe3bfd0f08a"
-                },
-                new User
-                {
-                    Id = 2, SteamName = "TestUser2", SteamId = "76561198078883932",
-                    AspNetUserId = "f7b530ec-bf89-45e1-8080-cbe3bfd0f08g"
-                },
-                new User
-                {
-                    Id = 3, SteamName = "TestUser3", SteamId = "76561198368539189",
-                    AspNetUserId = "f7b530ec-bf89-45e1-8080-cbe3bfd0f08h"
-                }
+            _poco = JsonSerializer.Deserialize<SteamUserPOCO>("{\"response\":{\"players\":[{\"steamid\":\"1\",\"communityvisibilitystate\":1,\"profilestate\":1,\"personaname\":\"Justin Davis\",\"commentpermission\":1,\"profileurl\":\"bogusProfileURL\",\"avatar\":\"bogusAvatarjpg\",\"avatarmedium\":\"bogusMediumjpg\",\"avatarfull\":\"bogusFullAvatarjpg\",\"avatarhash\":\"badHash\",\"lastlogoff\":1111111111,\"personastate\":1,\"primaryclanid\":\"1\",\"timecreated\":1111111111,\"personastateflags\":1,\"loccountrycode\":\"US\"}]}}");
+        }
 
-            };
-            _friends = new List<Friend>
-            {
-                new Friend { Id = 1, RootId = 1, SteamId = "76561198078883932" },
-                new Friend { Id = 2, RootId = 2, SteamId = "76561198280399190" },
-                new Friend { Id = 3, RootId = 2, SteamId = "76561198368539189" },
-            };
+        [Test]
+        public void User_WithNoId_IsInvalid()
+        {
+            // Arrange
+            User me = new User();
 
-            _userGameInfos = new List<UserGameInfo>()
-            {
-                new UserGameInfo { Id = 1, Followed = true, GameId = 1, Hidden = false, OwnerId = 1 },
-                new UserGameInfo { Id = 2, Followed = true, GameId = 2, Hidden = false, OwnerId = 1 },
-                new UserGameInfo { Id = 3, Followed = true, GameId = 2, Hidden = true, OwnerId = 2 },
-            };
-            _games = new List<Game>()
-            {
-                new Game { Id = 1, AppId = 310560, Name = "DiRT Rally" },
-                new Game { Id = 2, AppId = 218620, Name = "PAYDAY 2" },
-                new Game { Id = 2, AppId = 632360, Name = "Risk of Rain 2" },
-            };
+            // Act
+            me = MakeValidPerson();
+            ModelValidator mv = new ModelValidator(me);
 
-            _users.ForEach(p =>
-            {
-                p.Friends = _friends.Where(i => i.Id == p.Id).ToList();
-                p.UserGameInfos = _userGameInfos.Where(i => i.Id == p.Id).ToList();
-            });
+            // Assert
+            Assert.That(mv.Valid, Is.True);
+        }
 
-            // Finally, mock the context and dbsets
-            _mockContext = new Mock<SteamInfoDbContext>();
-            _mockGameInfoDbSet = MockHelpers.GetMockDbSet(_userGameInfos.AsQueryable());
-            _mockContext.Setup(ctx => ctx.UserGameInfos).Returns(_mockGameInfoDbSet.Object);
-            _mockContext.Setup(ctx => ctx.Set<UserGameInfo>()).Returns(_mockGameInfoDbSet.Object);
+        [Test]
+        public void TakeSteamPOCO_SetsPocoVariablesCorrectly ()
+        {
+            // Arrange
+            User me = new User();
+            me.Id = 1;
+            me.AspNetUserId = "1";
+
+            // Act
+            me.TakeSteamPOCO(_poco);
+
+            // Assert
+            Assert.Equals(me.SteamId, 1);
+            Assert.Equals(me.SteamName, "Justin Davis");
         }
     }
 }

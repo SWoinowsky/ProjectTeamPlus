e0cc0742698bc223fe0815c66ec937e3cf89d34b
Justin Davis <jdavis19@mail.wou.edu>
2023-03-18 02:07:23 -0700

    Added a test to valid the model.

--- src/NUnit_Tests/RepoTesting/UserLibraryVMTests.cs
+++ src/NUnit_Tests/RepoTesting/UserLibraryVMTests.cs
@@ -17,9 +17,48 @@ namespace NUnit_Tests.RepoTesting
 {
     public class UserLibraryVMTests
     {
+        private User MakeValidPerson()
+        {
+            
+            User newPerson = new User
+            {
+                Id = 1,
+                SteamId = "76561198053115178",
+                SteamName = "TestUser1",
+                Friends = new List<Friend>(),
+                UserAchievements = new List<UserAchievement>(),
+                UserGameInfos = new List<UserGameInfo>(),
+                AspNetUserId = "f7b530ec-bf89-45e1-8080-cbe3bfd0f08a"
+            };
+            return newPerson;
+        }
+        private UserLibraryVM MakeValidUserLibraryVM()
+        {
+            UserLibraryVM vm = new UserLibraryVM()
+            {
+                _user = MakeValidPerson(),
+                _games = new List<Game>()
+            };
+            return vm;
+        }
+
         [SetUp]
         public void Setup()
         {
+        }
+
+        [Test]
+        public void UserLibraryVM_isValid()
+        {
+            // Assert
+            UserLibraryVM vm = new UserLibraryVM();
+
+            // Act
+            vm = MakeValidUserLibraryVM();
+            ModelValidator mv = new ModelValidator(vm);
+
+            // Assert
+            Assert.That(mv.Valid, Is.True);
         }
     }
 }

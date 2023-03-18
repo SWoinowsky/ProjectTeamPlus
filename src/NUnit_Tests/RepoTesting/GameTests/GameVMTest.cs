b0b05866844bed11f687dcfb6c6fe9989af23a73
Justin Davis <jdavis19@mail.wou.edu>
2023-03-18 02:10:50 -0700

    Cleaned up testing file structure to help with navigating it.

--- src/NUnit_Tests/RepoTesting/GameTests/GameVMTest.cs
+++ src/NUnit_Tests/RepoTesting/GameTests/GameVMTest.cs
@@ -1,14 +1,4 @@
-using Microsoft.EntityFrameworkCore;
-using Moq;
 using SteamProject.Models;
-using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore;
-using System.Data.Common;
-using System.Diagnostics;
-using System.Net;
-using SteamProject.DAL.Abstract;
-using SteamProject.DAL.Concrete;
-using System;
 using SteamProject.ViewModels;
 using System.Text.Json;
 using SteamProject.Models.DTO;
--- src/NUnit_Tests/RepoTesting/UserTests/UserGameInfoTests.cs
+++ src/NUnit_Tests/RepoTesting/UserTests/UserGameInfoTests.cs
@@ -1,14 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Moq;
 using SteamProject.Models;
-using Microsoft.Data.Sqlite;
-using Microsoft.EntityFrameworkCore;
-using System.Data.Common;
-using System.Diagnostics;
-using System.Net;
 using SteamProject.DAL.Abstract;
 using SteamProject.DAL.Concrete;
-using System;
 
 namespace NUnit_Tests.RepoTesting
 {

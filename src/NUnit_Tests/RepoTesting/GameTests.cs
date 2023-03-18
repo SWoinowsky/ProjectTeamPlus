c1891c23219e9730a82c2ba0a6ebce6ae5df903a
Justin Davis <jdavis19@mail.wou.edu>
2023-03-18 00:08:23 -0700

    Tested for valid model with good data.

--- src/NUnit_Tests/RepoTesting/GameTests.cs
+++ src/NUnit_Tests/RepoTesting/GameTests.cs
@@ -16,10 +16,40 @@ namespace NUnit_Tests.RepoTesting
 {
     public class GameTests
     {
+
+        private Game MakeValidGame()
+        {
+            Game newGame = new Game
+            {
+                Id = 1,
+                AppId = 1,
+                Name = "TestUser1",
+                DescShort = "Short Description",
+                PlayTime = 100,
+                IconUrl = "bogusIconUrl",
+                LastPlayed = 100
+            };
+            return newGame;
+        }
+
         [SetUp]
         public void Setup()
         {
 
+        }
+
+        [Test]
+        public void Game_WithValidInfo_IsValid()
+        {
+            // Arrange
+            Game game = new Game();
+
+            // Act
+            game = MakeValidGame();
+            ModelValidator mv = new ModelValidator(game);
+
+            // Assert
+            Assert.That(mv.Valid, Is.True);
         }
     }
 }

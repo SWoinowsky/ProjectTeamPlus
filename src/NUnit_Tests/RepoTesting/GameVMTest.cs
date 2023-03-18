ae68bb2d0171e7b7a2dfeba825e8ae9bb193da77
Justin Davis <jdavis19@mail.wou.edu>
2023-03-18 01:00:33 -0700

    Added test to check for a valid GameVM.

--- src/NUnit_Tests/RepoTesting/GameVMTest.cs
+++ src/NUnit_Tests/RepoTesting/GameVMTest.cs
@@ -9,15 +9,48 @@ using System.Net;
 using SteamProject.DAL.Abstract;
 using SteamProject.DAL.Concrete;
 using System;
+using SteamProject.ViewModels;
+using SteamProject.Models.DTO;
 
 namespace NUnit_Tests.RepoTesting
 {
     public class GameVMTests
     {
+        private GameVM MakeValidGameVM()
+        {
+            GameVM gameVm = new GameVM{
+                _appId = 1,
+                _userGame = new UserGameInfo(),
+                _game = new Game{
+                    Id = 1,
+                    AppId = 1,
+                    Name = "A Game"
+                },
+                _poco = new GameInfoPOCO(),
+                playTime = 100
+            };
+            return gameVm;
+        }
         [SetUp]
         public void Setup()
         {
-            
+
         }
+
+        [Test]
+        public void GameVM_HasGame_isValid()
+        {
+            // Arrange
+            GameVM gameVm = MakeValidGameVM();
+
+            // Act
+            ModelValidator mv = new ModelValidator(gameVm);
+
+            // Assert
+            Assert.That(mv.Valid, Is.True);
+        }
+
+        
+
     }
 }

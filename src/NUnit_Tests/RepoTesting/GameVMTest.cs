a39dbcc4510faf98f62ea47cd840e72d39f6a582
Justin Davis <jdavis19@mail.wou.edu>
2023-03-18 00:42:08 -0700

    Added a file for GameVM testing.

---
+++ src/NUnit_Tests/RepoTesting/GameVMTest.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using SteamProject.Models;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Net;
+using SteamProject.DAL.Abstract;
+using SteamProject.DAL.Concrete;
+using System;
+
+namespace NUnit_Tests.RepoTesting
+{
+    public class GameVMTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+    }
+}

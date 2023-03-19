b70780c99876797e832e99c73aa7ec5f56bed964
Cole Woodard <cwoodard20@mail.wou.edu>
2023-03-19 12:20:28 -0700

    Updated task helper method function

--- src/SteamProject/Helpers/TaskHelperMethods.cs
+++ src/SteamProject/Helpers/TaskHelperMethods.cs
@@ -2,41 +2,22 @@
 
 namespace SteamProject.Helpers;
 
-public static class HelperMethods
+public class TaskHelperMethods
 {
-    public static string StripJunkFromString(string? str)
+    public static async Task<string[]> HandleFailedTasks(List<Task<string>> tasks)
     {
-        if (str == null)
+        var handledTasks = tasks.Select(task => task.ContinueWith(t =>
         {
-            return null;
-        }
+            if (t.IsFaulted)
+            {
+                return "Error: Task failed to complete";
+            }
+            else
+            {
+                return t.Result;
+            }
+        })).ToList();
 
-        //Remove Html tags from string ex: <a>
-        var item = Regex.Replace(str, @"<[^>]+>*", "");
-
-        //Remove braket tags ex: [a]
-        item = Regex.Replace(item, @"\[[^\]]+\]*", "");
-
-        //Remove hyperlinks ex: wwww, https
-        item = Regex.Replace(item, @"http[^\s]+", "");
-        item = Regex.Replace(item, @"www[^\s]+", "");
-
-        //Remove whatever is left that looks out of place
-        item = Regex.Replace(item, @"&#[^\s]+", "");
-        item = Regex.Replace(item, @"\{STEAM_CLAN_IMAGE\}/[A-Za-z0-9]+/[A-Za-z0-9]+\.[A-Za-z]+", "");
-        item = Regex.Replace(item, @"[A-Za-z]+&[A-Za-z0-9]+;s", "");
-        item = Regex.Replace(item, @"&[A-Za-z0-9]+;", "");
-
-
-        return item;
-    }
-
-    public static DateTime UnixTimeStampToDateTime(int unixTimeStamp)
-    {
-
-        // Unix timestamp is seconds past epoch
-        DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-        dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
-        return dateTime;
+        return await Task.WhenAll(handledTasks);
     }
 }

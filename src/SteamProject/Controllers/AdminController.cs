8210ddbbd6245debf778c1be666f8e118b75ba66
Justin Davis <jdavis19@mail.wou.edu>
2023-04-09 14:58:04 -0700

    Blank Admin Dashboard created and accessible.

---
+++ src/SteamProject/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SteamProject.DAL.Abstract;
+using SteamProject.Models;
+using Microsoft.AspNetCore.Identity;
+using SteamProject.Services;
+using Microsoft.AspNetCore.Authorization;
+using SteamProject.Models.DTO;
+using SteamProject.ViewModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace SteamProject.Controllers;
+
+public class AdminController: Controller
+{
+    [Authorize(Roles = "admin")]
+    public IActionResult Index(bool refresh)
+    {
+        return View();
+    }
+}

357ba74f77876060f31fef0cbbf4834933de519d
Justin Davis <jdavis19@mail.wou.edu>
2023-04-12 20:29:39 -0700

    Added an option to view banned Ids

--- src/SteamProject/Controllers/AdminController.cs
+++ src/SteamProject/Controllers/AdminController.cs
@@ -154,4 +154,9 @@ public class AdminController: Controller
     {
         throw new NotImplementedException();
     }
+
+    public IActionResult ViewBannedIds()
+    {
+        throw new NotImplementedException();
+    }
 }

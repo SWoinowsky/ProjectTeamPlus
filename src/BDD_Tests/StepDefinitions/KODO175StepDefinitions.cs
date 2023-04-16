fec8e30b123e9e16601bf0d75aa421987d4ccc1c
CarlosCortesEscobar <ccortesescobar19@mail.wou.edu>
2023-04-16 16:23:49 -0700

    creating feature on windows for mac

---
+++ src/BDD_Tests/StepDefinitions/KODO175StepDefinitions.cs
@@ -0,0 +1,33 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace BDD_Tests.StepDefinitions
+{
+    [Binding]
+    public class KODO175StepDefinitions
+    {
+        [Given(@"I have a friend on the friends page")]
+        public void GivenIHaveAFriendOnTheFriendsPage()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"I click on the name on the friend card")]
+        public void GivenIClickOnTheNameOnTheFriendCard()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I enter a nickname ""([^""]*)""")]
+        public void WhenIEnterANickname(string alias)
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"I should see them references as ""([^""]*)""")]
+        public void ThenIShouldSeeThemReferencesAs(string alias)
+        {
+            throw new PendingStepException();
+        }
+    }
+}

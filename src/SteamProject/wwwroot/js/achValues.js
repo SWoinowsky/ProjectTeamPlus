6293d8967e390c7b0033eaf0afc06a860f0af5b4
Carlos Cortes Escobar <ccortesescobar19@mail.wou.edu>
2023-04-23 13:25:13 -0700

    rating system and assigning complete

--- src/SteamProject/wwwroot/js/achValues.js
+++ src/SteamProject/wwwroot/js/achValues.js
@@ -22,39 +22,62 @@ document.addEventListener('DOMContentLoaded', () => {
         return newAch
     }
 
-    const assignValues = (cards, gaps) => {
-        cards.forEach((card) => {
+    const assignValues = (achCards, gaps) => {
+        achCards.forEach((card) => {
             let achName = card.getElementsByClassName('card-title')[0].textContent
-            
-            card.
+
             gaps.forEach((gap) => {
                 if (gap.name == achName) {
-                    let val = gap.percent
+                    let val = gap.percentage
+                    let flare = card.getElementsByClassName('flare')[0]
                     switch (true) {
                         case val >= 90:
                             // iron
-                            card.setAttribute("class", "iron")
-                            // card.appendChild("")
-
+                            flare.setAttribute("class", "iron-flare")
+                            flare.style.display = "block"
+                            card.setAttribute("class", "card achievementCard iron")
+                            card.getElementsByClassName('card-body')[0].getElementsByClassName('ach-value')[0].innerText = "1"
                             break;
-                        case val >= 75 && val <= 89:
+
+                        case val >= 75 && val <= 89.9:
                             // gold
-                            break
+                            flare.setAttribute("class", "gold-flare")
+                            flare.style.display = "block"
+                            card.setAttribute("class", "card achievementCard gold")
+                            card.getElementsByClassName('card-body')[0].getElementsByClassName('ach-value')[0].innerText = "3"
+                            break;
 
-                        case val >= 60 && val <= 74:
+                        case val >= 60 && val <= 74.9:
                             //plat
+                            flare.setAttribute("class", "plat-flare")
+                            flare.style.display = "block"
+                            card.setAttribute("class", "card achievementCard plat")
+                            card.getElementsByClassName('card-body')[0].getElementsByClassName('ach-value')[0].innerText = "5"
                             break;
 
-                        case val >= 35 && val <= 59:
+                        case val >= 35 && val <= 59.9:
                             //diamond
+                            flare.setAttribute("class", "diamond-flare")
+                            flare.style.display = "block"
+                            card.setAttribute("class", "card achievementCard diamond")
+                            card.getElementsByClassName('card-body')[0].getElementsByClassName('ach-value')[0].innerText = "7"
                             break;
 
-                        case val >= 6 && val <= 34:
+                        case val >= 1.1 && val <= 34.9:
                             //emerald
+                            flare.setAttribute("class", "emerald-flare")
+                            flare.style.display = "block"
+                            card.setAttribute("class", "card achievementCard emerald")
+                            card.getElementsByClassName('card-body')[0].getElementsByClassName('ach-value')[0].innerText = "9"
                             break;
 
-                        case val <= 5:
+                        case val <= 1:
                             //ruby
+                            card.getElementsByClassName('flare')[0]
+                            flare.setAttribute("class", "ruby-flare")
+                            flare.style.display = "block"
+                            card.setAttribute("class", "card achievementCard ruby")
+                            card.getElementsByClassName('card-body')[0].getElementsByClassName('ach-value')[0].innerText = "11"
                             break;
 
                         default:
@@ -75,6 +98,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 .then((data) => {
                     let gap = data
                     gapNew = genObj(schema, gap)
+                    assignValues(cards, gapNew)
                 })
         })
     }

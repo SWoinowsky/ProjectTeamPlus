cc82477f87d227e7f64282539b0be965d56687e1
Justin Davis <jdavis19@mail.wou.edu>
2023-05-21 22:06:44 -0700

    Added in needed functions for getting all of the speed runs for a given competition, and then one for converting the runTime variable to a TimeSpan instead of the string for comparison.

--- src/SteamProject/DAL/Abstract/ISpeedRunRepository.cs
+++ src/SteamProject/DAL/Abstract/ISpeedRunRepository.cs
@@ -4,5 +4,7 @@ namespace SteamProject.DAL.Abstract;
 
 public interface ISpeedRunRepository : IRepository<SpeedRun>
 {
+    List<SpeedRun> GetAllSpeedRunsForComp(int compId);
+    TimeSpan ConvertRunToTimeSpan(string runTime);
     IEnumerable<string> GetSpeedRunsForValidation();
 }
--- src/SteamProject/DAL/Concrete/SpeedRunRepository.cs
+++ src/SteamProject/DAL/Concrete/SpeedRunRepository.cs
@@ -10,6 +10,23 @@ public class SpeedRunRepository : Repository<SpeedRun>, ISpeedRunRepository
     {
     }
 
+    public List<SpeedRun> GetAllSpeedRunsForComp(int compId)
+    {
+        return GetAll().Where(s => s.CompetitionId == compId).ToList();
+    }
+
+    public TimeSpan ConvertRunToTimeSpan(string runTime)
+    {
+        string[] components = runTime.Split(':');
+        
+        int hours = int.Parse(components[0]);
+        int minutes = int.Parse(components[1]);
+        int seconds = int.Parse(components[2]);
+        int milliseconds = int.Parse(components[3]);
+        
+        return new TimeSpan(0, hours, minutes, seconds, milliseconds);
+    }
+
     public IEnumerable<string> GetSpeedRunsForValidation()
     {
         return null;
